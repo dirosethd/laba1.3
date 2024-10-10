ced83d4e863b091c433f3255680ea866543f3307
dirose <dirose@DESKTOP-PQA70RD>
2024-10-10 15:08:09 +0200

    Добавьте файлы проекта.

---
+++ laba1.3/Program.cs
@@ -0,0 +1,16 @@
+﻿try
+{
+    Console.Write("Введите трехзначное число: ");
+    int x = int.Parse(Console.ReadLine());
+        int hundreds = x / 100;
+        int tens = (x / 10) % 10;
+        int units = x % 10;
+
+        int z = tens * 100 + hundreds * 10 + units;
+
+        Console.WriteLine("Новое число: " + z);
+}
+catch
+{
+    Console.WriteLine("Ошибка: Введите корректное число.");
+}
